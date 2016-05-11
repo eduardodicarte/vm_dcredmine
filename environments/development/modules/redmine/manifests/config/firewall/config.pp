205ad14f175fec7aac83deaa83b21e004d622d8a
Eduardo Dicarte <eduardo_dicarte@yahoo.com.br>
2016-05-11 17:54:35 -0300

    Configurando o IPTABLES para inserir a regra uma única vez.

--- environments/development/modules/redmine/manifests/config/firewall/config.pp
+++ environments/development/modules/redmine/manifests/config/firewall/config.pp
@@ -1,12 +1,14 @@
 class redmine::config::firewall::config{
-  exec{"libera_trafego_tcp_input_80":
-    command => "iptables -t filter -p tcp -I INPUT 1 -j ACCEPT",
-    path => "/usr/sbin/"
+  exec{'libera_trafego_tcp_input_80':
+    command => 'iptables -t filter -p tcp -I INPUT 1 -j ACCEPT -m comment --comment "Liberacao da porta 80"',
+    path => ['/usr/sbin/','/usr/bin/'],
+    unless => 'iptables -L | grep "Liberacao da porta 80"'
   }
   
   exec{"iptables_save_rules":
     command => "service iptables save",
-    path => "/usr/sbin/",
-    require => Exec["libera_trafego_tcp_input_80"]
+    path =>  ['/usr/sbin/','/usr/bin/'],
+    unless => 'iptables -L | grep "Liberacao da porta 80"',
+    require => Exec["libera_trafego_tcp_input_80"],
   }
 }
